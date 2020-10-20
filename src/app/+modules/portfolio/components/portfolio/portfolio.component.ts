64508737822e9d623f95de57d28885e47c89fb9a
Angel <asuarezc91@gmail.com>
2020-10-20 17:07:28 +0200

    filters added in portfolio page

--- src/app/+modules/portfolio/components/portfolio/portfolio.component.ts
+++ src/app/+modules/portfolio/components/portfolio/portfolio.component.ts
@@ -11,18 +11,34 @@ export class PortfolioComponent implements OnInit {
 
   closeResult: string;
   name: number; 
+  show: string; 
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.name = 1; 
+    this.show = 'all';
   }
 
   openLg(content2) {
     this.modalService.open(content2, { size: 'lg', centered: true, windowClass: 'dark-modal' });
   }
 
+  react(){
+    this.show  = 'react'; 
+  }
 
+  javascript(){
+    this.show  = 'javascript'; 
+  }
+
+  angular(){
+    this.show  = 'angular'; 
+  }
+
+  all(){
+    this.show  = 'all'; 
+  }
 
 
 
